92b94329431e0bf79d953e783e47c9a597d573fe
Matthieu Tisserand <matthieu.tisserand@polytechnique.org>
2023-04-27 17:02:13 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.34

---
+++ src/GoogleTakeOuter.jl
@@ -0,0 +1,5 @@
+module GoogleTakeOuter
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GoogleTakeOuter
+using Test
+
+@testset "GoogleTakeOuter.jl" begin
+    # Write your tests here.
+end
